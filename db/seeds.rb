ffccc50eb81461b0f3b2574c316146a246765632
Leo GENUIT <genuitleo@gmail.com>
2021-08-24 12:17:00 +0200

    wip seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,37 @@ user = User.create!(email: "toto@gmail.com",
   )
 end
 
+10.times do
+  Message.create!(
+    sender_email: "contact@fnac.com",
+    content: Faker::Lorem.paragraph,
+    user: user
+  )
+end
+
+8.times do
+  Message.create!(
+    sender_email: "contact@darty.com",
+    content: Faker::Lorem.paragraph,
+    user: user
+  )
+end
+
+8.times do
+  Message.create!(
+    sender_email: "contact@figaro.com",
+    content: Faker::Lorem.paragraph,
+    user: user
+  )
+end
+
+5.times do
+  Message.create!(
+    sender_email: "contact@carrefour.com",
+    content: Faker::Lorem.paragraph,
+    user: user
+  )
+end
+
+
 puts "#{Message.count} messages are available !"
