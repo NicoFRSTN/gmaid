03a524fffdc87e3e33b93a0b6ce12097f38d0dc9
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-25 00:20:39 +0200

    working google login box

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def GoogleAuth
 
     access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)*
+    user = User.from_omniauth(access_token)
     session[:user_id] = user.id
     user.google_token = access_token.credentials.token
     p user
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get 'auth/google_oauth2/callback', to: 'sessions#GoogleAuth'
   get 'auth/failure', to: redirect('/')
 
+
 end
