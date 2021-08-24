06fd6ad6ddd408b3e359278b143900345d8003bd
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-24 17:58:03 +0200

    ominauth part 1

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['490583260220-sj8lig89teselefiu7p0srhk5iti6jla.apps.googleusercontent.com'], ENV['
+Mn7kyHcEGyZy_sBssaB-KyYb
+']
+#
+end
