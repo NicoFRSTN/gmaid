da833240d31e273dafb09d7c5c2b73ea6d44c416
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-25 14:28:04 +0200

    working gmail login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,20 @@ class User < ApplicationRecord
   has_many :messages
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
 
-
-  def self.from_omniauth(auth)
-    p auth.info
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.email = auth.info.email
-      user.encrypted_password = SecureRandom.hex
+  # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      user = User.create(email: data['email'],
+      email: data['email'],
+      password: Devise.friendly_token[0,20]
+          )
     end
+    user
   end
 end
