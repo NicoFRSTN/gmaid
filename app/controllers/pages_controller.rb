5ab3e858913dc5c8f04b24de870ec4bca238d8b3
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-08-26 22:43:04 +0200

    Improve dashboard with category for each big sender

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,5 +12,11 @@ class PagesController < ApplicationController
     query = "SELECT sender_email, COUNT(*) AS number FROM messages GROUP BY sender_email ORDER BY number DESC LIMIT 10"
 
     @biggest_senders = ActiveRecord::Base.connection.execute(query).to_a
+
+    @biggest_senders.each do |category|
+      match = category['sender_email'].match(/^.*@(?<domain_name>.*)\..*$/)
+      category['company'] = match[:domain_name].upcase
+    end
+    puts "bug:  #{@biggest_senders}"
   end
 end
