6d3169dbf102f1af0980736cb7106c4d3f86d66c
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-27 17:13:08 +0200

    commit before heroku push

--- app/services/gmail_api.rb
+++ app/services/gmail_api.rb
@@ -11,9 +11,25 @@ class GmailApi
     uri = URI.parse(endpoint)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    puts http.get(uri.request_uri, headers).body
+    result = JSON.parse(http.get(uri.request_uri, headers).body)
+    result['messages'].each do |message|
+      details = get_details_for(message['id'])
+      user.messages.create(object: details[:Subject], sender_email: details[:From], date: details[:Date], content: details[:snippet])
+    end
     binding.pry
-    # puts JSON.parse(Net::HTTP.get(URI(uri), headers))
+  end
+
+  def self.get_details_for(message_id)
+    # ici on tape l'API de nouveau
+    endpoint = "https://gmail.googleapis.com/gmail/v1/users/nicolas.feuerstein%40gmail.com/messages/#{message_id}/trash?key=AIzaSyDen2EikLCReQegow6qnJDvIFUrtd1niXI"
+    headers = {}
+    headers["Authorization"] = "Bearer #{user.google_token}"
+    headers["Accept"] = "application/json"
+    puts headers
+    uri = URI.parse(endpoint)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    result = JSON.parse(http.get(uri.request_uri, headers).body)
   end
 
 end
