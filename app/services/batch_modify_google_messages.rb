0ac904daa629c6b339484f169319af0f55350f69
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-09-01 13:52:13 +0200

    added batch methods

---
+++ app/services/batch_modify_google_messages.rb
@@ -0,0 +1,37 @@
+require "uri"
+require "net/http"
+
+class BatchGoogleModify
+  def initialize(user, message_id)
+    @email = user.email
+    @bearer_token = user.google_token
+    @message_id = message_id
+  end
+
+
+  def call
+    uri = URI("https://gmail.googleapis.com/gmail/v1/users/#{@email}/messages/batchModify?key=AIzaSyDen2EikLCReQegow6qnJDvIFUrtd1niXI")
+
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer #{@bearer_token}"
+    request["Accept"] = "application/json"
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    request.body = JSON.dump({
+  "addLabelIds" =>
+  ],
+  "ids" =>
+  ],
+  "removeLabelIds" =>
+  ]
+})
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  end
+end
