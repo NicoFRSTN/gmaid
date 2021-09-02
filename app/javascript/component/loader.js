1f66b4a8ad7b81a7dbd1a56bc46c92b3aeb47152
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-09-02 21:57:14 +0200

    whole sync div is clickable

--- app/javascript/component/loader.js
+++ app/javascript/component/loader.js
@@ -7,6 +7,7 @@ const initLoader = () => {
   const spinner = document.querySelector('.fas')
   const text = document.querySelector('.sync-button-text')
   const shader = document.querySelector('#shader')
+  const link = document.querySelector('.sync-button-link')
 
   console.log(bouton)
 
@@ -17,6 +18,7 @@ const initLoader = () => {
     spinner.classList.toggle('fas-loading')
     text.classList.toggle('sync-button-text-loading')
     text.innerHTML="Synchronizing. Please wait..."
+    link.classList.toggle('sync-button-link-loading')
     shader.style.display='block'
 
 
