cce7f7330ccb3c09a74f23708830773365f16a62
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2021-09-03 16:58:51 +0200

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,19 @@
-require 'faker'
+domains = ["github.com",
+        "accounts.google.com",
+        "linkedin.com",
+        "itr.mail.codecademy"]
 
-Message.destroy_all
-User.destroy_all
 
-user = User.create!(email: "toto@gmail.com",
-                    password: "azerty"
-                   )
+domains.each do |domain|
+  emails = Message.where("messages.from ILIKE ?", "%#{domain}%")
 
-  # t.string "from"
-  #   t.string "subject"
-  #   t.datetime "date_at"
-  #   t.string "snippet"
 
-30.times do
-  Message.create!(
-    google_id: 3,
-    from: Faker::Internet.email,
-    subject: 'pub',
-    snippet: Faker::Lorem.paragraph,
-    user: user
-  )
+  nb = emails.count
+
+  nb_to_delete = (nb / 2) - rand(3)
+
+  nb_to_delete = 0 if nb_to_delete < 0
+
+
+  emails.limit(nb_to_delete).destroy_all
 end
-
-10.times do
-  Message.create!(
-    google_id: 3,
-    from: "contact@fnac.com",
-    subject: 'newsletter fnac',
-    snippet: Faker::Lorem.paragraph,
-    user: user
-  )
-end
-
-8.times do
-  Message.create!(
-    google_id: 3,
-    from: "contact@darty.com",
-    snippet: Faker::Lorem.paragraph,
-    user: user
-  )
-end
-
-8.times do
-  Message.create!(
-    google_id: 3,
-    from: "contact@figaro.com",
-    snippet: Faker::Lorem.paragraph,
-    user: user
-  )
-end
-
-5.times do
-  Message.create!(
-    google_id: 3,
-    from: "contact@carrefour.com",
-    snippet: Faker::Lorem.paragraph,
-    user: user
-  )
-end
-puts "#{Message.count} messages are available !"
